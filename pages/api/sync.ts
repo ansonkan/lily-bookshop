4ff789f879d0eb06117f9bc82484268ce8b9d492
Anson Kan <ansonkan@tutanota.com>
2022-12-29 00:36:32 +0800

    add a new api endpoint to debug directus webhook events

---
+++ pages/api/sync.ts
@@ -0,0 +1,20 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import type { NextApiRequest, NextApiResponse } from 'next'
+
+import { Logger } from 'aws-amplify'
+
+const logger = new Logger('sync')
+
+export default function handler(req: NextApiRequest, res: NextApiResponse) {
+  logger.debug('debugging directus webhook events: ', {
+    headers: req.headers,
+    body: req.body,
+    cookies: req.cookies,
+    method: req.method,
+    url: req.url,
+    preview: req.preview,
+    previewData: req.previewData,
+  })
+
+  res.status(200)
+}
