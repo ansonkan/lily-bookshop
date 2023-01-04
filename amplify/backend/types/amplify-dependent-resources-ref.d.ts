d69381bd49a961dc158cf8e25e528a9496cb5f95
Anson Kan <ansonkan@tutanota.com>
2023-01-04 16:11:08 +0800

    ran amplify pull after add auth + storage on Amplify studio UI

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,20 @@
+export type AmplifyDependentResourcesAttributes = {
+  auth: {
+    lilybookshop: {
+      AppClientID: 'string'
+      AppClientIDWeb: 'string'
+      CreatedSNSRole: 'string'
+      IdentityPoolId: 'string'
+      IdentityPoolName: 'string'
+      UserPoolArn: 'string'
+      UserPoolId: 'string'
+      UserPoolName: 'string'
+    }
+  }
+  storage: {
+    s3lilybookshopstorage135156f8: {
+      BucketName: 'string'
+      Region: 'string'
+    }
+  }
+}
